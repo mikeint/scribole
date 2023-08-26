0c0bd898f8ae23ee6196e4f85b25516152150de0
Michael Sansone <only.sansone@gmail.com>
2023-08-26 16:06:00 -0400

    adding rooster theme

--- client/src/components/TopBar/TopBar.js
+++ client/src/components/TopBar/TopBar.js
@@ -1,23 +1,13 @@
  
 import { Link } from 'react-router-dom';
-import './TopBar.scss';
-
-import h1 from '../../files/images/h1.png';
-import h2 from '../../files/images/h2.png';
-import h3 from '../../files/images/h3.png';
-import h4 from '../../files/images/h4.png';
+import './TopBar.scss'; 
 
 const TopBar = () => {
 
     return (
         <div className="topBar">
             <Link to='/games' className='logo'></Link>
-            <div className='imageCycler'>
-                <img className="fadeCycle fadeCycle-1" src={h1} alt={h1} />
-                <img className="fadeCycle fadeCycle-2" src={h2} alt={h2} />
-                <img className="fadeCycle fadeCycle-3" src={h3} alt={h3} />
-                <img className="fadeCycle fadeCycle-4" src={h4} alt={h4} />
-            </div>
+            <div className='eggGif'></div>
         </div>
     );
 };
--- client/src/pages/Games/Games.js
+++ client/src/pages/Games/Games.js
@@ -13,32 +13,32 @@ const Games = () => {
             <div className="gamesContainer">
 
                 <Link to="/game1" className="game game1">
-                    <div class="playGameText">
+                    <div className="playGameText">
                        PLAY
                     </div>
                 </Link>
                 <Link to="/game2" className="game game2">
-                    <div class="playGameText">
+                    <div className="playGameText">
                        PLAY
                     </div>
                 </Link>
                 <Link to="/game3" className="game game3">
-                    <div class="playGameText">
+                    <div className="playGameText">
                        PLAY
                     </div>
                 </Link>
                 <Link to="/game4" className="game game4">
-                    <div class="playGameText">
+                    <div className="playGameText">
                        PLAY
                     </div>
                 </Link>
                 <Link to="/game5" className="game game5">
-                    <div class="playGameText">
+                    <div className="playGameText">
                        PLAY
                     </div>
                 </Link>
                 <Link to="/game6" className="game game6">
-                    <div class="playGameText">
+                    <div className="playGameText">
                        PLAY
                     </div>
                 </Link>
